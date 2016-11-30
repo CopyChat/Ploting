f36cc8b0121717039714de5fa1c0657baa0a7c1e
ctang <ctang@mbp-de-ctang.home>
2016-12-01 01:08:43 +0400

    normal updata

--- Python/one_curve_txt.meteofrance.py
+++ Python/one_curve_txt.meteofrance.py
@@ -6,6 +6,7 @@
 ########################################
 # http://gfesuite.noaa.gov/developer/netCDFPythonInterface.html
 
+import os
 import netCDF4
 import pylab as pl
 import numpy as np
@@ -23,26 +24,18 @@ pl.close('all')
 ########################## OBS location:
 
 # all the plot file shoud be here:
-OBSDIR='/Users/ctang/climate/GLOBALDATA/OBSDATA/SSR_changes_SWIO'
-
-ProjOBSnc=['NCEP-NCAR','ERA_Interim',\
-        'CM_SAF-CDR','CM_SAF-SARAH-E','CERES']
-
-ProjOBStxt=['HelioClim-1']
-
-OBSncfile=[\
-        'dswrf.sfc.gauss.yearmean.fldmean.1948-2015.swio.nc',\
-        'ERA.ssrd.ssr.ssrc.yearmean.1979-2014.fldmean.swio.nc',\
-        'SISmm.CDR.yearmean.fldmean.198301-200512.swio.nc',\
-        'SISmm.SARAH-E.yearmean.fldmean.199901-201512.swio.nc',\
-        'rsds_CERES-EBAF_L3B_Ed2-8_2001-2013.swio.fldmean.yearmean.nc']
+OBSDIR='/Users/ctang/climate/GLOBALDATA/OBSDATA/Meteo-France/39site'
         
-OBStxtfile=[\
-        'rsds.HelioClim-1.1985-2005.txt']
 
-VARIABLE=['dswrf','ssrd','SIS','SIS','rsds']
+ProjOBStxt=['MeteoFrance']
 
-COLORtar=['black','dodgerblue','deeppink','darkgreen',\
+OBStxtfile=['montly.missing.removed']
+
+# to prepare the plot file
+#os.environ['OBStxtfile']=str(OBStxtfile[0])
+#os.system(" awk 'NR>1{print $2,$6*(10000/3600/24/30)}' $OBStxtfile > time.wm2.temp")
+
+COLOR=['black','dodgerblue','deeppink','darkgreen',\
         'brown','chocolate',\
         'green','yellowgreen','aqua','olive','teal',\
         'blue','purple','darkmagenta','fuchsia','indigo',\
@@ -59,16 +52,16 @@ print "==============================================="
 fig,ax = plt.subplots(figsize=(16,9))
 plt.xlabel('Year',fontsize=16)  
 plt.ylabel('Surface Downwelling Solar Radiation ( W/m2 )',fontsize=16)
-plt.title("Surface Downwelling Solar Radiation over the SWIO",fontsize=18)
+plt.title("monthly mean of SSR over Reunion Island",fontsize=18)
 
 # vertical range ylim yrange
-plt.ylim(215,255)
-plt.yticks(np.arange(160,300, 20))
+plt.ylim(0,275)
+plt.yticks(np.arange(0,275, 20))
 
 #plt.xlim(1948,2016)
 plt.grid()
 
-ax.xaxis.set_major_locator(YearLocator(5)) # interval = 5
+ax.xaxis.set_major_locator(MonthLocator(1)) # interval = 5
 ax.xaxis.set_major_formatter(DateFormatter('%Y-%m'))
 ax.fmt_xdata = DateFormatter('%Y')
 
@@ -78,46 +71,10 @@ ax.fmt_xdata = DateFormatter('%Y')
 
 #=================================================== 3 windows
 #plt.axvspan(1950, 1980, alpha=0.2, color='teal')
-#plt.axvspan(1980, 2000, alpha=0.2, color='teal')
-#plt.axvspan(2000, 2016, alpha=0.2, color='teal')
 #=================================================== draw lines
 #plt.axvline(x=2005,linewidth=2, color='gray')
-plt.axhline(y=0,linewidth=2, color='gray')
+plt.axhline(y=39,linewidth=2, color='gray')
 
-
-##### to plot nc file
-for obs in ProjOBSnc:
-
-    infile1=OBSDIR+'/'+OBSncfile[ProjOBSnc.index(obs)]
-    print('the file is == ' +infile1)
-
-    #open input files
-    infile=IO.NetCDFFile(infile1,'r')
-
-    # read the time to datetime
-    TIME=netCDF4.num2date(infile.variables['time'][:],\
-            infile.variables['time'].units,\
-            calendar=infile.variables['time'].calendar)
-
-    #TIME=[t.year for t in TIME]
-    #TIME=[t.strftime("%Y-%m") for t in TIME]
-    #TIME=mpl.dates.date2num(TIME)
-
-    # read the variable
-    SSR=infile.variables[VARIABLE[ProjOBSnc.index(obs)]][:,0,0].copy()
-
-#=================================================== to plot
-    print "======== to plot =========="
-
-    print 'NO. of year:',len(TIME)
-    print TIME
-    print SSR
-
-    #plt.plot_date(mdates.datestr2num(TIME),SSR)
-    plt.plot(TIME,SSR,\
-        label=obs,\
-        color=COLORtar[ProjOBSnc.index(obs)],\
-        linewidth=2)
 
 
 ##### to plot the text file:
@@ -126,7 +83,7 @@ for obs in ProjOBStxt:
     infile1=OBSDIR+'/'+OBStxtfile[ProjOBStxt.index(obs)]
     print('the file is == ' +infile1)
 
-    TIME,SSR= np.loadtxt(infile1, unpack=True,\
+    TIME,SSR,Histo= np.loadtxt(infile1, unpack=True,\
             converters={ 0: mdates.strpdate2num('%Y-%m')})
 
     print " plotting text file ================== "
@@ -134,11 +91,22 @@ for obs in ProjOBStxt:
     print SSR
 
     plt.plot(TIME,SSR,\
+        'o-',\
         label=obs,\
-        color=COLORtar[ProjOBStxt.index(obs)+len(ProjOBSnc)],\
+        color=COLOR[ProjOBStxt.index(obs)+len(ProjOBStxt)],\
         linewidth=2)
 
+##### to plot the number of data
 
+    plt.bar(TIME,Histo,20,alpha=0.4)
+
+plt.text(TIME[32],65,'number of records, 39 in total',\
+        size=16,rotation=0.,
+        ha="center",va="center",
+        bbox = dict(boxstyle="round",
+            ec=(1., 0.5, 0.5),
+            fc=(1., 0.8, 0.8),
+            ))
 fig.autofmt_xdate()
 plt.legend(loc=2)
 plt.show()
